3f0a3568baec363c685f8644080f93c490514ebf
aniket-703 <148362183+aniket-703@users.noreply.github.com>
2024-03-05 21:41:26 +0530

    Update collectdata.py
    make the code shorted and remove the same multiple lines and replaced it with the for loop

--- collectdata.py
+++ collectdata.py
@@ -1,116 +1,64 @@
 import cv2
 import os
 
-directory = 'Dataset_250'
-print(os.getcwd())
+directory = 'D:/Final year project/Dataset_250'
 
 if not os.path.exists(directory):
     os.mkdir(directory)
-if not os.path.exists(f'{directory}/blank'):
-    os.mkdir(f'{directory}/blank')
 
+# Create directories for letters
 for i in range(65, 91):
     letter = chr(i)
     if not os.path.exists(f'{directory}/{letter}'):
         os.mkdir(f'{directory}/{letter}')
 
+# Create directories for digits
+for i in range(48, 58):
+    digit = chr(i)
+    if not os.path.exists(f'{directory}/{digit}'):
+        os.mkdir(f'{directory}/{digit}')
+
+# Create directory for blank
+if not os.path.exists(f'{directory}/blank'):
+    os.mkdir(f'{directory}/blank')
+
 cap = cv2.VideoCapture(0)
+
 while True:
     _, frame = cap.read()
-    count = {
-        'a': len(os.listdir(directory + "/A")),
-        'b': len(os.listdir(directory + "/B")),
-        'c': len(os.listdir(directory + "/C")),
-        'd': len(os.listdir(directory + "/D")),
-        'e': len(os.listdir(directory + "/E")),
-        'f': len(os.listdir(directory + "/F")),
-        'g': len(os.listdir(directory + "/G")),
-        'h': len(os.listdir(directory + "/H")),
-        'i': len(os.listdir(directory + "/I")),
-        'j': len(os.listdir(directory + "/J")),
-        'k': len(os.listdir(directory + "/K")),
-        'l': len(os.listdir(directory + "/L")),
-        'm': len(os.listdir(directory + "/M")),
-        'n': len(os.listdir(directory + "/N")),
-        'o': len(os.listdir(directory + "/O")),
-        'p': len(os.listdir(directory + "/P")),
-        'q': len(os.listdir(directory + "/Q")),
-        'r': len(os.listdir(directory + "/R")),
-        's': len(os.listdir(directory + "/S")),
-        't': len(os.listdir(directory + "/T")),
-        'u': len(os.listdir(directory + "/U")),
-        'v': len(os.listdir(directory + "/V")),
-        'w': len(os.listdir(directory + "/W")),
-        'x': len(os.listdir(directory + "/X")),
-        'y': len(os.listdir(directory + "/Y")),
-        'z': len(os.listdir(directory + "/Z")),
-        'blank': len(os.listdir(directory + "/blank"))
-    }
+    
+    # Count files in each directory
+    count_letters = {chr(i).lower(): len(os.listdir(f'{directory}/{chr(i)}')) for i in range(65, 91)}
+    count_digits = {chr(j): len(os.listdir(f'{directory}/{chr(j)}')) for j in range(48, 58)}
+    count_blank = len(os.listdir(os.path.join(directory, 'blank')))
 
-    row = frame.shape[1]
-    col = frame.shape[0]
     cv2.rectangle(frame, (0, 40), (300, 300), (255, 255, 255), 2)
     cv2.imshow("data", frame)
     frame_roi = frame[40:300, 0:300]
     cv2.imshow("ROI", frame_roi)
     frame_resized = cv2.resize(frame_roi, (250, 250))
-    interrupt = cv2.waitKey(10)
-    if interrupt & 0xFF == ord('a'):
-        cv2.imwrite(os.path.join(directory + '/A/' + str(count['a']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('b'):
-        cv2.imwrite(os.path.join(directory + '/B/' + str(count['b']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('c'):
-        cv2.imwrite(os.path.join(directory + '/C/' + str(count['c']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('d'):
-        cv2.imwrite(os.path.join(directory + '/D/' + str(count['d']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('e'):
-        cv2.imwrite(os.path.join(directory + '/E/' + str(count['e']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('f'):
-        cv2.imwrite(os.path.join(directory + '/F/' + str(count['f']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('g'):
-        cv2.imwrite(os.path.join(directory + '/G/' + str(count['g']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('h'):
-        cv2.imwrite(os.path.join(directory + '/H/' + str(count['h']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('i'):
-        cv2.imwrite(os.path.join(directory + '/I/' + str(count['i']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('j'):
-        cv2.imwrite(os.path.join(directory + '/J/' + str(count['j']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('k'):
-        cv2.imwrite(os.path.join(directory + '/K/' + str(count['k']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('l'):
-        cv2.imwrite(os.path.join(directory + '/L/' + str(count['l']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('m'):
-        cv2.imwrite(os.path.join(directory + '/M/' + str(count['m']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('n'):
-        cv2.imwrite(os.path.join(directory + '/N/' + str(count['n']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('o'):
-        cv2.imwrite(os.path.join(directory + '/O/' + str(count['o']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('p'):
-        cv2.imwrite(os.path.join(directory + '/P/' + str(count['p']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('q'):
-        cv2.imwrite(os.path.join(directory + '/Q/' + str(count['q']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('r'):
-        cv2.imwrite(os.path.join(directory + '/R/' + str(count['r']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('s'):
-        cv2.imwrite(os.path.join(directory + '/S/' + str(count['s']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('t'):
-        cv2.imwrite(os.path.join(directory + '/T/' + str(count['t']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('u'):
-        cv2.imwrite(os.path.join(directory + '/U/' + str(count['u']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('v'):
-        cv2.imwrite(os.path.join(directory + '/V/' + str(count['v']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('w'):
-        cv2.imwrite(os.path.join(directory + '/W/' + str(count['w']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('x'):
-        cv2.imwrite(os.path.join(directory + '/X/' + str(count['x']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('y'):
-        cv2.imwrite(os.path.join(directory + '/Y/' + str(count['y']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('z'):
-        cv2.imwrite(os.path.join(directory + '/Z/' + str(count['z']) + '.jpg'), frame_resized)
-    if interrupt & 0xFF == ord('.'):
-        cv2.imwrite(os.path.join(directory + '/blank/' + str(count['blank']) + '.jpg'), frame_resized)
+
+    interrupt = cv2.waitKey(25)
     if interrupt & 0xFF == 13:  # 13 is the ASCII code for 'Enter' key
         break
+    
+    if interrupt & 0xFF in range(48, 58):
+        digit = chr(interrupt & 0xFF)
+        cv2.imwrite(os.path.join(directory, digit, f'{count_digits[digit]+1}.jpg'), frame_resized)
+        print(f"Saved image for {digit} with count {count_digits[digit]+1}")
+        count_digits[digit] += 1
+        
+    
+    elif interrupt & 0xFF in range(65, 91) or interrupt & 0xFF in range(97, 123):
+        letter = chr(interrupt & 0xFF).upper()
+        cv2.imwrite(os.path.join(directory, letter, f'{count_letters[letter.lower()]+1}.jpg'), frame_resized)
+        print(f"Saved image for {letter} with count {count_letters[letter.lower()]+1}")
+        count_letters[letter.lower()] += 1
+
+    elif interrupt & 0xFF == ord('.'):
+        cv2.imwrite(os.path.join(directory, 'blank', f'{count_blank+1}.jpg'), frame_resized)
+        print(f"Saved image for BLANK with count {count_blank+1}")
+        count_blank += 1
 
 cap.release()
 cv2.destroyAllWindows()
